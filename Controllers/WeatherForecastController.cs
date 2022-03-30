07307d8f9d9ca2ee53dbf855de0e78c78915bbfd
frmauro <frmauro8@gmail.com>
2022-03-30 17:02:32 -0300

    many changes 4

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Cool2", "Cool3"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Cool2", "Cool3", "Cool4"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
