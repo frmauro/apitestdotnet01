d66d8e0cd1525346903c219dce7d247567e1a321
frmauro <frmauro8@gmail.com>
2022-04-01 17:04:51 -0300

    many changes again- 04

--- deploytst.sh
+++ deploytst.sh
@@ -6,25 +6,25 @@
 ########################################################
 
 
-result=$( sudo docker images -q apitest01 )
-if [[ -n "$result" ]]; then
-echo "image exists"
-sudo docker rmi -f apitest01
-else
-echo "No such image"
-fi
+# result=$( sudo docker images -q apitest01 )
+# if [[ -n "$result" ]]; then
+# echo "image exists"
+# sudo docker rmi -f apitest01
+# else
+# echo "No such image"
+# fi
 
-echo "build the docker image"
+# echo "build the docker image"
 
-echo "built docker images and proceeding to delete existing container"
-result=$( docker ps -q -f name=apitest01 )
-if [[ $? -eq 0 ]]; then
-echo "Container exists"
-sudo docker container rm -f apitest01
-echo "Deleted the existing docker container"
-else
-echo "No such container"
-fi
+# echo "built docker images and proceeding to delete existing container"
+# result=$( docker ps -q -f name=apitest01 )
+# if [[ $? -eq 0 ]]; then
+# echo "Container exists"
+# sudo docker container rm -f apitest01
+# echo "Deleted the existing docker container"
+# else
+# echo "No such container"
+# fi
 
 sudo docker build -t apitest01 .
 
@@ -32,6 +32,6 @@ echo "built docker images and proceeding to delete existing container"
 
 echo "Deploying the updated container"
 
-sudo docker run -d -p 5272:5272 --name apitest01  apitest01
+#sudo docker run -d -p 5272:5272 --name apitest01  apitest01
 
 echo "Deploying the container"
