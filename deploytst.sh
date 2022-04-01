7e0e7d3c25b8054e16a1b0829d1e77068928ceb2
frmauro <frmauro8@gmail.com>
2022-04-01 15:01:02 -0300

    many changes again- 03

---
+++ deploytst.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+########################################################
+
+## Shell Script to Build Docker Image and run.   
+
+########################################################
+
+
+result=$( sudo docker images -q apitest01 )
+if [[ -n "$result" ]]; then
+echo "image exists"
+sudo docker rmi -f apitest01
+else
+echo "No such image"
+fi
+
+echo "build the docker image"
+
+echo "built docker images and proceeding to delete existing container"
+result=$( docker ps -q -f name=apitest01 )
+if [[ $? -eq 0 ]]; then
+echo "Container exists"
+sudo docker container rm -f apitest01
+echo "Deleted the existing docker container"
+else
+echo "No such container"
+fi
+
+sudo docker build -t apitest01 .
+
+echo "built docker images and proceeding to delete existing container"
+
+echo "Deploying the updated container"
+
+sudo docker run -d -p 5272:5272 --name apitest01  apitest01
+
+echo "Deploying the container"
